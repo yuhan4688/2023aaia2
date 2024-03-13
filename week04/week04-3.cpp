1b1fb3d01d5436323be4d9aacd8b601707f00c3e
yuahn4688 <12750131@gmail.com>
2024-03-13 11:10:41 +0800

    week04

---
+++ week04/week04-3.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+  int a=3;
+
+  while(a>0)
+  {
+      printf("a=%d\n",a);
+      a--;///就是a-1的意思
+  }
+
+}
---
+++ week04/week04-4.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+  int a=3;
+
+  while(a>0)
+  {
+      printf("a=%d\n",a);
+      a--;///就是a-1的意思
+  }
+  for(int b=3; b>0; b--) {
+    printf("b是%d\n",b);
+    }
+}
