b15feb70861034fd4f16ba496eca6104295a1786
yuhan4688 <12750131@gmail.com>
2024-03-27 11:03:36 +0800

    week06

---
+++ week06/week06-3.cpp
@@ -0,0 +1,12 @@
+//A030 week06-3.cpp
+#include <stdio.h>
+int main()
+{
+	int a[100];
+	for(int i=0;i<100;i++){
+		scanf("%d",&a[i]);
+	}
+	for(int i=99;i>=0;i--){
+		printf("%d\n",a[i]);
+	}
+}
---
+++ week06/week06-3.py
@@ -0,0 +1,5 @@
+#A030 week06-3.py
+a=list(map(int,input().split()))
+
+for i in range(99,-1,-1):
+	print(a[i])
