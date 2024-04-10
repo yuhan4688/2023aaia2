d00fa78647be8d066987a1558510f95420a3e29d
yuhan4688 <12750131@gmail.com>
2024-04-10 11:44:13 +0800

    week08

---
+++ week08/week08-4.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ( void )
+{
+	int h,day=0;
+	printf("請輸入蝸牛欲爬行的高度：");
+	scanf("%d",&h);
+	while(h>0){
+		day++;
+		h-=5;
+		if(h<0) break;
+		h += 2;
+	}
+    printf("%d天可爬到頂端",day);
+}
