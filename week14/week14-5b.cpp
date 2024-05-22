9e33f8090db4c2d03dfdab4324e3f7bea17de826
yuahn4688 <12750131@gmail.com>
2024-05-22 12:05:46 +0800

    week14

---
+++ week14/week14-5b.cpp
@@ -0,0 +1,12 @@
+//soit107 advance 015
+#include <stdio.h>
+int main()
+{
+	int a,b;
+	scanf("%d %d ",&a,&b);
+	
+	for(int i=a;i<=b;i++){
+		if(i%7==0) printf("%d ",i);
+	}
+
+}
---
+++ week14/week14-5c.cpp
@@ -0,0 +1,15 @@
+//soit106-advance-002
+#include <stdio.h>
+int gcd(int a,int b) {
+	if(a==0) return b;
+	if(b==0) return a;
+	return gcd(b,a%b);
+}
+
+int main()
+{
+	int a,b;
+	scanf("%d %d ",&a,&b);
+	int c=gcd(a,b);
+	printf("%d %d\n",a/c,b/c);
+}
