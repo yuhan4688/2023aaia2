7b051ecc6ab570524621ef6e5800108a2bede3f7
yuhan4688 <12750131@gmail.com>
2024-06-12 10:02:50 +0800

    week17

---
+++ week17/week17-1c.cpp
@@ -0,0 +1,25 @@
+///week17-1a.cpp 三合一，泡泡排序法
+#include <stdio.h>
+void myPrint(int a[10]){
+    for(int i=0;i<10;i++){
+        printf("%d ",a[i]);
+    }
+    printf("\n");
+}
+int main()
+
+{
+    int a[10]={9,8,7,6,5,4,3,2,1,0};
+    myPrint(a);
+    for(int k=0;k<10;k++){
+            for(int i=0;i<10-1;i++){
+
+            if(a[i]>a[i+1]){
+                int temp=a[i];
+                a[i]=a[i+1];
+                a[i+1]=temp;
+            }
+        }
+        myPrint(a);
+    }
+}
